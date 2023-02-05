ae4e007fd43d92f82f8f9e48abd40518a1fc1344
e3ham <99908651+e3ham@users.noreply.github.com>
2023-02-05 10:39:25 -0500

    First commit
    bug with the vector implementation in CARD.h

---
+++ Card.cpp
@@ -0,0 +1,186 @@
+#include "Card.h"
+
+Card::Card()
+{}
+
+Card::~Card()
+{}
+
+Card::Card(const Card & card) {
+    this->cardTypes = *new vector<string>(card.cardTypes);
+    this->choice = new string(*(card.choice));
+}
+
+void Card::initialType() 
+{}
+
+vector<string> *Card::getpointer() {
+    return &cardTypes;
+}
+
+void Card::printVectorType() {
+    cout << "The card types are: " <<endl;
+    for (int i = 0; i < cardTypes.size(); i++) {
+        cout << cardTypes.at(i) << endl;
+    }
+}
+
+void Card::setCardType(int i) {
+    choice = &cardTypes.at(i);
+}
+
+string *Card::getCardType() {
+    return choice;
+}
+
+Deck::Deck()
+{}
+
+Deck::~Deck() {
+    delete(pointerCard);
+    delete(tempCard);
+}
+
+Deck::Deck(const Deck &deck) {
+
+    this->deck = *new vector<Card*> (deck.deck);
+    this->pointerCard = new Card(*(deck.pointerCard));
+    this->tempCard = new Card(*(deck.tempCard));
+}
+
+Deck & Deck::operator=(const Deck &deck) {
+    this->deck = *new vector<Card*>(deck.deck);
+    this->pointerCard = new Card(*(deck.pointerCard));
+    this->tempCard = new Card(*(deck.tempCard));
+    return *this;
+}
+
+void Deck::initialDeck() {
+
+    for (int i = 0; i < 30; i++) {
+        pointerCard = new Card;
+        if (i < 5) {
+            pointerCard -> setCardType(0);
+            deck.push_back(pointerCard);
+        }
+        if (i >= 5 && i < 10) {
+			pointerCard->setCardType(1);
+			deck.push_back(pointerCard);
+		}
+		if (i >= 10 && i < 15) {
+			pointerCard->setCardType(2);
+			deck.push_back(pointerCard);
+		}
+		if (i >= 15 && i < 20) {
+			pointerCard->setCardType(3);
+			deck.push_back(pointerCard);
+		} 
+        if (i >= 20 && i < 25) {
+            pointerCard->setCardType(4);
+			deck.push_back(pointerCard);
+        } 
+        if (i >= 25 && i < 30) {
+            pointerCard->setCardType(5);
+			deck.push_back(pointerCard);
+        }
+    }
+    cout << ("Deck is initialized\n") <<endl;
+}
+
+void Deck::printDeckSize() {
+    cout << (" The deck currently contains ") << deck.size() << (" cards. \n") << endl;
+}
+
+Card* Deck::draw() {
+    int temp = (rand() % deck.size());
+    tempCard = deck.at(temp);
+
+    deck.erase(deck.begin() + temp);
+
+    return tempCard;
+}
+
+void Deck::addCard(Card *add_card) {
+    deck.push_back(add_card);
+}
+
+Hand::Hand() 
+{}
+
+Hand::~Hand()
+{}
+
+Hand::Hand(const Hand &hand) {
+    this->handCards = *new vector<Card*>(hand.handCards);
+    this->playingCards = *new vector<Card*>(hand.playingCards);
+}
+
+Hand &Hand::operator=(const Hand &hand) {
+    this->handCards = *new vector<Card*>(hand.handCards);
+    this->playingCards = *new vector<Card*>(hand.playingCards);\
+    return *this;
+}
+
+void Hand::setHand(Card *card) {
+    handCards.push_back(card);
+}
+
+void Hand::printHand() {
+    if (handCards.size() > 0) {
+        cout << (" The Hand cards has ") << handCards.size() << (" cards ") << endl;
+        for (int i = 0; i < handCards.size(); i++) {
+            cout << "  # " << i << " is " << *handCards.at(i)->getCardType() << endl;
+        }
+    } else {
+		cout << (" the hand cards is empty \n") << endl;
+	}
+}
+
+void Hand::printPlayingCard() {
+    if (playingCards.size() > 0) {
+        cout << (" the play cards order is: ") << endl;
+        for (int i = 0; i < playingCards.size(); i++) {
+            cout << "  # " << i << " is " << *playingCards.at(i)->getCardType() << endl;
+        }
+    } else {
+		cout << (" the hand cards is empty \n") << endl;
+	}
+}
+
+void Hand::play(Card* card, Deck* deck) {
+    playingCards.push_back(card);
+    cardToDeck(deck);
+
+    removePlayingFromHand(card);
+
+    playingCards.pop_back();
+}
+
+void Hand::cardToDeck(Deck* deck) {
+    for (int i = 0; i < playingCards.size(); i++) {
+        deck->addCard(playingCards.at(i));
+    }
+}
+
+vector<Card*>* Hand::getHand() {
+    return &handCards;
+}
+
+vector<Card*>* Hand::getPlayingCard() {
+    return &playingCards;
+}
+
+void Hand::removePlayingFromHand(Card* card) {
+    for (int i = 0; i < handCards.size(); i++) {
+        if (*handCards.at(i)->getCardType() == *card->getCardType()) {
+            handCards.erase(handCards.begin() + i);
+            cout << ("...deleting the card ") << *card->getCardType() << (" of the Hand cards...\n")<<  endl;
+			return;
+        }
+    }
+}
+
+void Hand::deletePlayingCards() {
+    playingCards.clear();
+    cout<< "\n vec_play_cards are cleaned." <<endl;
+}
