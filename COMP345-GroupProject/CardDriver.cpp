9b39f63114e803283269f0ccf26a105e5cf6cabd
Tala Sleman <92806751+talasleman@users.noreply.github.com>
2023-02-12 23:22:23 -0500

    Add files via upload

---
+++ COMP345-GroupProject/CardDriver.cpp
@@ -0,0 +1,43 @@
+#include "Card.h"
+#include "Card.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    Deck deck;
+
+    deck.initialDeck();
+
+    Hand hand;
+
+    deck.printDeckSize();
+
+    cout << "Assigning 5 cards randomly into the hand deck\n"
+         << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        hand.setHand(deck.draw());
+    }
+
+    hand.printHand();
+    deck.printDeckSize();
+
+    cout << (" test to play all Cards of the HandCards...then remove it from HandCards and put the cards into vec_deck_cards \n") << endl;
+    int hand_size = hand.getHand()->size();
+    for (int k = 0; k < hand_size; k++)
+    {
+        hand.play(hand.getHand()->at(0), &deck);
+    } // because the erase method of the vector will auto move the cards
+
+    // print the playing cards
+    hand.printPlayingCard();
+    // print the current hand cards
+    hand.printHand();
+
+    // test the size of current deck
+    deck.printDeckSize();
+}
