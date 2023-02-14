24971bace7e37a45133af6a947b48e4aa9c2c325
Tala Sleman <92806751+talasleman@users.noreply.github.com>
2023-02-13 22:50:15 -0500

    Update Player.h

--- COMP345-GroupProject/Player.h
+++ COMP345-GroupProject/Player.h
@@ -1,28 +1,30 @@
 #pragma once
 #include <vector>
-#include "Map.h"
-#include "Card.h"
-#include "Orders.h"
+#include "Map.cpp"
+#include "Card.cpp"
+#include "Orders.cpp"
 
 class Player
 {
 private:
-    OrderList *OL();
-    std::vector<Territory *> territories;
+    OrderList *OL;
     Hand *hand;
     std::string *playerName;
+    std::vector<Territory *> toDefendList;
+    std::vector<Territory *> toAttackList;
 
     // Divide territories method
     void divideTerritories();
 
 public:
     // Constructor
-    Player(std::string) {}
+    Player(std::string playerName);
 
     // Destructor
     ~Player();
 
     // ToDefend and ToAttack methods
+    std::vector<Territory *> territories;
     std::vector<Territory *> toDefend();
     std::vector<Territory *> toAttack();
 
@@ -30,13 +32,13 @@ public:
     void issueOrder(std::string);
 
     // Accessors
-    std::string getName() const { return *name; }
+    std::string getName() const { return *playerName; }
     std::vector<Territory *> getTerritories() const { return territories; }
     OrderList *getOL() const { return OL; }
     Hand *getHand() const { return hand; }
 
     // Mutators
-    void setName(const std::string &newName) { *name = newName; }
+    void setName(const std::string &newName) { *playerName = newName; }
     void setTerritories(const std::vector<Territory *> &newTerritories) { territories = newTerritories; }
     void setOL(OrderList *newOL) { OL = newOL; }
     void setHand(Hand *newHand) { hand = newHand; }
