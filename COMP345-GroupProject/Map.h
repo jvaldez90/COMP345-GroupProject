6845f1b67cfb52353116daf6dcbc0060c8da29ef
Lorenzo001 <34139780+Lorenzo001@users.noreply.github.com>
2023-03-16 12:07:26 -0400

    Add files via upload

---
+++ COMP345-GroupProject/Map.h
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+
+using namespace std;
+class Player;
+
+class Territory {
+private:
+    string name;
+    int id;
+    int x;
+    int y;
+    int continentId;
+    int numberOfArmies;
+    Player* player;
+    vector<Territory*> adjacentTerritories;
+public:
+    Territory(int id, const string& name, int continentId, int x, int y);//Constructor
+    Territory();//Default constructor
+    Territory(const Territory& territory); // Copy constructor
+    int getId() const;
+    int getContinentId() const;
+    int getNumberOfArmies() const;
+    void addAdjacentTerritory(Territory* territory);
+    void addArmies(int armies);
+    void deleteArmies(int armies);
+    void setNumberOfArmies(int armies);
+    vector<Territory*> get_neighbour_territory();
+    int getX() const;
+    int getY() const;
+    Player* getPlayer();
+    void setPlayer(Player* player);
+    const string& getName() const;
+    friend std::ostream& operator << (std::ostream& out, const Territory* territory);// Stream insertion operator
+    Territory& operator=(const Territory & territory); // Assignment operator
+};
+
+
+class Continent {
+private:
+    int id;
+    string name;
+    string color;
+    vector<Territory*> territories;
+    int numberOfTerritories;
+public:
+    Continent(int id, const string& name, const string& color);// constructor
+    Continent(const Continent& continent);//Copy constructor
+    int getId() const;
+    const string& getName() const;
+    const string& getColor() const;
+    int getNumberOfTerritories() const;
+    void addTerritory(Territory* territory);
+    bool isEmpty() const;
+    const vector<Territory*>& getTerritories() const;
+    friend std::ostream& operator << (std::ostream& out, const Continent* continent);// Stream insertion operator
+    Continent& operator=(const Continent &continent);    // Assignment operator
+    
+};
+
+class Map {
+private:
+    vector<Continent*> continents;
+    vector<Territory*> territories;
+    string name;
+public:
+    Map(string name);//Contructor
+    Map(const Map& map); // Copy constructor
+    void addTerritory(Territory* territory);
+    void addContinent(Continent* continent);
+    void dfs(Territory* territory, bool visited[], int& number_of_visited_territories);
+    Territory* getTerritoryById(int TerritoryId);
+    Continent* getContinentById(int continentId);
+    bool validate(Map* map);
+    bool map_is_connected();
+    bool territory_is_unique();
+    bool continents_is_connected_subgraph();
+    const string& getName() const;
+    friend std::ostream& operator<<(std::ostream& out, Map* map);// Stream insertion operator
+    Map& operator=(const Map &map);// Assignment operator
+};
+
+class MapLoader {
+public:
+    Map*  load(const string &filename);
+    MapLoader();
+};
+#pragma once
