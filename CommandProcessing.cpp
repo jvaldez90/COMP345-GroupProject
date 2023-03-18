89a88ac165b1b7f2e8755d59ee39e396e548a231
e3ham <99908651+e3ham@users.noreply.github.com>
2023-03-18 18:14:23 -0400

    Update CommandProcessing.cpp

--- CommandProcessing.cpp
+++ CommandProcessing.cpp
@@ -1,52 +1,118 @@
-#pragma once
+#include "CommandProcessing.h"
+#include "GameEngine.h"
 
-#include <string>
+#include <iostream>
+#include <sstream>
+#include <regex>
 #include <vector>
 #include <fstream>
 using namespace std;
 
-class Command {
-private:
-    std::string commandString;
-    vector<string> effectString;
-    string name;
+CommandProcessor::CommandProcessor() {}
 
-public:
-    void outputCommand(const Command& cmd);
-    string getName();
-    Command(const std::string& name);
-    std::string getCommandString() const;
-    void saveEffect(const std::string& effStr);
-    std::string getEffectString() const;
-};
 
-class CommandProcessor {
-private:
-    std::vector<Command> commands;
+std::string CommandProcessor::readCommand() {
+    std::string cmdStr;
+    std::cout << "Enter command: ";
+    std::getline(std::cin, cmdStr);
+    return cmdStr;
+}
 
-    std::string readCommand();
-    void saveCommand(const std::string& cmdStr);
+string outputCommand(Command cmd) {
+    //cout << "Command: " << cmd.getName() << endl;
+    return cmd.getName();
+}
 
-public:
-    std::string getName(const Command& cmd);
-    std::vector<std::string> getEffects(const Command& cmd);
-    void outputCommand(const Command& cmd);
-    CommandProcessor();
-    void validate(Command cmd);
-    Command getCommand();
-};
+void Command::saveEffect(const std::string& effect) {
+    effectString.push_back(effect);
+}
 
-class FileCommandProcessorAdapter {
-private:
-    std::vector<Command> commands;
-    std::string filename;
-    std::ifstream inputFileStream;
+void CommandProcessor::saveCommand(const std::string& cmdStr) {
+    Command cmd(cmdStr);
+    commands.push_back(cmd);
+}
 
-    std::string readCommandFromFile();
-    void saveCommand(const std::string& cmdStr);
+void CommandProcessor::validate(Command cmd) {
+    // Call the appropriate game state function to check if the command is valid
+    // and save any error message in the Command object's effect.
 
-public:
-    FileCommandProcessorAdapter(const std::string& filename);
-    void validate();
-    Command getCommand();
-};
+    if (cmd.getName().compare(GameEngine::command)) {
+        if (cmd.getName() == "validatemap") {
+            if (GameEngine::command == "maploaded") {
+                cmd.saveEffect("Command executed successfully.");
+                std::cout << "Effect: " << cmd.getEffectString() << std::endl;
+            }
+        } else if (cmd.getName() == "gamestart") {
+            if (GameEngine::command == "playersadded") {
+                cmd.saveEffect("Command executed successfully.");
+                std::cout << "Effect: " << cmd.getEffectString() << std::endl;
+            }
+        } else if (cmd.getName() == "replay") {
+            if (GameEngine::command == "win") {
+                cmd.saveEffect("Command executed successfully.");
+                std::cout << "Effect: " << cmd.getEffectString() << std::endl;
+            }
+        } else if (cmd.getName() == "quit") {
+            if (GameEngine::command == "win") {
+                cmd.saveEffect("Command executed successfully.");
+                std::cout << "Effect: " << cmd.getEffectString() << std::endl;
+            }
+        } else if (cmd.getName() == "addplayer") {
+            if (GameEngine::command == "mapvalidated" || GameEngine::command == "playersadded") {
+                cmd.saveEffect("Command executed successfully.");
+                std::cout << "Effect: " << cmd.getEffectString() << std::endl;
+            }
+        } else if (cmd.getName() == "loadmap") {
+            if (GameEngine::command == "start" || GameEngine::command == "maploaded") {
+                cmd.saveEffect("Command executed successfully.");
+                std::cout << "Effect: " << cmd.getEffectString() << std::endl;
+            }
+        }
+
+    }
+
+
+}
+
+
+Command CommandProcessor::getCommand() {
+    std::string cmdStr = readCommand();
+    saveCommand(cmdStr);
+    return commands.back();
+}
+
+FileCommandProcessorAdapter::FileCommandProcessorAdapter(const std::string& filename) :
+        filename(filename), inputFileStream(filename) {
+    if (!inputFileStream.is_open()) {
+        std::cerr << "Error: could not open file " << filename << std::endl;
+    }
+}
+
+std::string FileCommandProcessorAdapter::readCommandFromFile() {
+    std::string cmdStr;
+    std::getline(inputFileStream, cmdStr);
+    return cmdStr;
+}
+
+void FileCommandProcessorAdapter::saveCommand(const std::string& cmdStr) {
+    Command cmd(cmdStr);
+    commands.push_back(cmd);
+}
+
+void FileCommandProcessorAdapter::validate() {
+    // Call the appropriate game state function to check if the command is valid
+    // and save any error message in the Command object's effect.
+}
+
+Command FileCommandProcessorAdapter::getCommand() {
+    std::string cmdStr = readCommandFromFile();
+    saveCommand(cmdStr);
+    return commands.back();
+}
+
+Command::Command(const std::string& cmdStr) : commandString(cmdStr) {}
+
+std::string Command::getCommandString() const {
+    return commandString;
+}
+
