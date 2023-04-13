216eb653fea657aeedc9dd3d48037f9fab6ba3db
e3ham <99908651+e3ham@users.noreply.github.com>
2023-04-13 16:24:56 -0400

    Update CommandProcessor.h

--- COMP345-GroupProject/CommandProcessor.h
+++ COMP345-GroupProject/CommandProcessor.h
@@ -1,55 +1,76 @@
-#include "GameEngine.h"
+#include "CommandProcessor.cpp"
 
+#include <iostream>
 #include <string>
-#include <vector>
-#include <fstream>
-using namespace std;
 
-#ifndef COMMANDPROCESSOR_H
-#define COMMANDPROCESSOR_H
+int main(int argc, char *argv[])
+{
 
-class Command {
-private:
-    string commandString;
-    string name;
+    if (argc == 2 && std::string(argv[1]) == "-console")
+    {
+        // Use the console-based CommandProcessor
+        CommandProcessor cp;
+        while (true)
+        {
+            Command cmd = cp.getCommand();
+            // Validate the command and execute it
+            cp.validate(cmd);
+            cmd.saveEffect("Command executed successfully.");
+            // std::cout << "Effect: " << getEffectString(cmd) << std::endl;
+            return 0;
+        }
+    }
+    else if (argc == 3 && std::string(argv[1]) == "-file")
+    {
+        // Create a new instance of the adapter and pass in the name of the command file
+        FileCommandProcessorAdapter adapter("commands.txt");
 
-public:
-    Command();
-    string effectString;
-    bool hasArgument(string arg) const;
-    void outputCommand(const Command& cmd);
-    string getName(Command cmd);
-    Command(const string name);
-    void saveEffect(const std::string& effStr);
-    string getEffectString(Command& cmd) const;
-};
+        // Process all commands from the file
+        adapter.processCommands();
 
+        return 0;
+    }
+    else if (argc == 9 && std::string(argv[1]) == "tournament")
+    {
+        string mapArg = "";
+        string playerArg = "";
+        int games = 0;
+        int maxTurns = 0;
 
+        for (int i = 1; i < argc; i++)
+        {
+            if (argv[i][0] == '-')
+            {
+                switch (argv[i][1])
+                {
+                case 'M':
+                    mapArg = argv[i + 1];
+                    break;
+                case 'P':
+                    playerArg = argv[i + 1];
+                    break;
+                case 'G':
+                    games = stoi(argv[i + 1]);
+                    break;
+                case 'D':
+                    maxTurns = stoi(argv[i + 1]);
+                    break;
+                default:
+                    cerr << "Invalid argument." << endl;
+                    return 1;
+                }
+            }
 
-class CommandProcessor : public Command {
-private:
-    vector<Command> commands;
-    string readCommand();
-    void saveCommand(const std::string& cmdStr);
-
-public:
-    //std::vector<std::string> getEffects(const Command& cmd);
-    //void outputCommand(const Command& cmd);
-    CommandProcessor();
-    void validate(Command cmd);
-    Command getCommand();
-    bool CommandProcessor::isValidTournamentCommand(Command cmd);
-};
-
-
-
-class FileCommandProcessorAdapter : public CommandProcessor {
-public:
-    FileCommandProcessorAdapter(const string& fileName);
-    void processCommands();
-
-private:
-    ifstream fileStream;
-};
-
-#endif
+            if (mapArg.empty() || playerArg.empty() || games == 0 || maxTurns == 0)
+            {
+                cerr << "Invalid arguments." << endl;
+                return 1;
+            }
+        }
+    }
+    else
+    {
+        std::cerr << "Usage: " << argv[0] << " -console | -file <filename> | tournament -M <listofmapfiles> -P <listofplayerstrategies> -G <numberofgames> -D <maxnumberofturns>" << std::endl;
+        return 1;
+    }
+}
