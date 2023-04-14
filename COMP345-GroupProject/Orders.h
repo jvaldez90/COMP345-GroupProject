aad2322e44953b1ab52803bb6552cd9152895552
Tala Sleman <92806751+talasleman@users.noreply.github.com>
2023-04-14 11:22:03 -0400

    Update Orders.h

--- COMP345-GroupProject/Orders.h
+++ COMP345-GroupProject/Orders.h
@@ -9,320 +9,391 @@
 
 using namespace std;
 
-//Forward declaration
+// Forward declaration
 class Player;
 class Territory;
 
-class Order : public Subject, public ILoggable {
+class Order : public Subject, public ILoggable
+{
 
 private:
-
-    string* description; //pointer to a string containing the order description
-    string* effect; //pointer to a string containing the order's effect
-    Player* player; //pointer to the player issuing the order
+    string *description; // pointer to a string containing the order description
+    string *effect;      // pointer to a string containing the order's effect
+    Player *player;      // pointer to the player issuing the order
 
 public:
-
-    //Default constructor to initialize order.
+    // Default constructor to initialize order.
     Order();
 
-    //Constructor to initialize an order with its description and effect
-    Order(const string& description, const string& effect);
+    // Constructor to initialize an order with its description and effect
+    Order(const string &description, const string &effect);
 
-    //A parameterized constructor which initializes an order with the provided description, effect and issuing player
-    Order(const string& description, const string& effect, Player& player);
+    // A parameterized constructor which initializes an order with the provided description, effect and issuing player
+    Order(const string &description, const string &effect, Player &player);
 
+    // Copy constructor
+    Order(const Order &order);
 
-    //Copy constructor
-    Order(const Order& order);
-
-    //Destructor to avoid memory leaks
+    // Destructor to avoid memory leaks
     virtual ~Order();
 
-    //Getter for the description of the order
-    virtual string* getDescription() const ;
+    // Getter for the description of the order
+    virtual string *getDescription() const;
 
-    //Getter for the effect of the order
-    virtual string* getEffect() const ;
+    // Getter for the effect of the order
+    virtual string *getEffect() const;
 
-    //Getter for the player issuing the order
-    virtual Player* getPlayer() const ;
+    // Getter for the player issuing the order
+    virtual Player *getPlayer() const;
 
-    //Setter for the description of the order
-    virtual void setDescription(const string& description);
+    // Setter for the description of the order
+    virtual void setDescription(const string &description);
 
-    //Setter for the effect of the order
-    virtual void setEffect(const string& effect);
+    // Setter for the effect of the order
+    virtual void setEffect(const string &effect);
 
-    //Setter for the player issuing the order
-    virtual void setPlayer(Player& player);
+    // Setter for the player issuing the order
+    virtual void setPlayer(Player &player);
 
-    //Validate method to check if order if valid
-    virtual bool validate()=0;
+    // Validate method to check if order if valid
+    virtual bool validate() = 0;
 
-    //Execute method to execute valid orders
-    virtual bool execute()=0;
+    // Execute method to execute valid orders
+    virtual bool execute() = 0;
 
-    //Assignment operator definition
-    Order& operator=(const Order& order);
+    // Assignment operator definition
+    Order &operator=(const Order &order);
 
-    // Operator << definition 
-    friend ostream& operator<<(ostream& strm, const Order& order);
-
+    // Operator << definition
+    friend ostream &operator<<(ostream &strm, const Order &order);
 };
 
-class Deploy : public Order {
+class Deploy : public Order
+{
 
 private:
-
-    Territory* target;
+    Territory *target;
     int armies;
 
 public:
-
-    //Default constructor for Deploy order object
+    // Default constructor for Deploy order object
     Deploy();
 
     /*Parameterized constructor for deploy order containing information on player calling deploy order, its target
     and the number of armies deployed */
-    Deploy(Player& player, int& armies, Territory& target);
+    Deploy(Player &player, int &armies, Territory &target);
 
-    //Destructor
+    // Destructor
     ~Deploy();
 
-    //Copy constructor
-    Deploy(const Deploy& deploy);
+    // Copy constructor
+    Deploy(const Deploy &deploy);
 
-    //validate method override from base class
+    // validate method override from base class
     bool validate() override;
 
-    //execute method from base class
+    // execute method from base class
     bool execute() override;
 
-    //Assignment operator definition
-    Deploy& operator=(const Deploy& deploy);
+    // Assignment operator definition
+    Deploy &operator=(const Deploy &deploy);
 
-    //Operator << definition
-    friend ostream& operator<<(std::ostream& strm, const Deploy& deploy);
+    // Operator << definition
+    friend ostream &operator<<(std::ostream &strm, const Deploy &deploy);
 
-    // Override stringToLog class from Order
-    string stringToLog() const override;
-
+    // Modifications made by Tala
+    inline void Notify(ILoggable *deploy) override
+    {
+        LogObserver logObserver;
+        logObserver.Update(deploy);
+    }
+    // Modifications made by Tala
+    inline std::string stringToLog() override
+    {
+        std::cout << "Deploy order executed." << std::endl;
+        return "Deploy order executed.\n";
+    }
 };
 
-class Advance : public Order {
+class Advance : public Order
+{
 
 private:
-    Territory* source;
-    Territory* target;
+    Territory *source;
+    Territory *target;
     int armies;
 
 public:
-    //Default constructor for Deploy order object
+    // Default constructor for Deploy order object
     Advance();
 
-    //Destructor
+    // Destructor
     ~Advance();
 
-    //Copy constructor
-    Advance(const Advance& advance);
+    // Copy constructor
+    Advance(const Advance &advance);
 
     /*Parameterized constructor for advance order containing information on player
     giving the order, the territory from which the order comes from, and the target territory*/
-    Advance(Player& player, int& armies, Territory& source, Territory& target);
+    Advance(Player &player, int &armies, Territory &source, Territory &target);
 
-    //validate method override from base class
+    // validate method override from base class
     bool validate() override;
 
-    //execute method from base class
+    // execute method from base class
     bool execute() override;
 
-    //Assignment operator definition
-    Advance& operator=(const Advance& advance);
+    // Assignment operator definition
+    Advance &operator=(const Advance &advance);
 
-    //Operator << definition
-    friend ostream& operator<<(std::ostream& strm, const Advance& advance);
+    // Operator << definition
+    friend ostream &operator<<(std::ostream &strm, const Advance &advance);
 
-    // Override stringToLog class from Order
-    string stringToLog() const override;
-
+    // Modifications made by Tala
+    inline void Notify(ILoggable *advance) override
+    {
+        LogObserver logObserver;
+        logObserver.Update(advance);
+    }
+    // Modifications made by Tala
+    inline std::string stringToLog() override
+    {
+        std::cout << "Advance order executed." << std::endl;
+        return "Advance order executed.\n";
+    }
 };
 
-class Blockade : public Order {
+class Blockade : public Order
+{
 
 private:
-    Territory* target;
+    Territory *target;
 
 public:
-    //Default constructor
+    // Default constructor
     Blockade();
 
-    //Parameterized Constructor containing information on player calling the blockade and its target
-    Blockade(Player& player, Territory& target);
+    // Parameterized Constructor containing information on player calling the blockade and its target
+    Blockade(Player &player, Territory &target);
 
-    //Copy constructor
-    Blockade(const Blockade& blockade);
+    // Copy constructor
+    Blockade(const Blockade &blockade);
 
-    //Destructor
+    // Destructor
     ~Blockade();
 
-    //Override validate method from base class
+    // Override validate method from base class
     bool validate() override;
 
-    //Override execute method from base class
+    // Override execute method from base class
     bool execute() override;
 
-    //Defining the output operator
-    friend ostream& operator<<(ostream& strm, const Blockade& blockade);
+    // Defining the output operator
+    friend ostream &operator<<(ostream &strm, const Blockade &blockade);
 
-    //Defining the assignment operator
-    Blockade& operator=(const Blockade& blockade);
+    // Defining the assignment operator
+    Blockade &operator=(const Blockade &blockade);
 
-    // Override stringToLog class from Order
-    string stringToLog() const override;
+    // Modifications made by Tala
+    inline void Notify(ILoggable *blockade) override
+    {
+        LogObserver logObserver;
+        logObserver.Update(blockade);
+    }
+    // Modifications made by Tala
+    inline std::string stringToLog() override
+    {
+        std::cout << "Blockade order executed." << std::endl;
+        return "Blockafe order executed.\n";
+    }
 };
 
-class Airlift : public Order {
+class Airlift : public Order
+{
 
 private:
-    Territory* source;
-    Territory* target;
+    Territory *source;
+    Territory *target;
     int armies;
 
 public:
-    //Default constructor
+    // Default constructor
     Airlift();
 
-    //Parameterized Constructor
-    Airlift(Player& player, int& armies, Territory& source, Territory& target);
+    // Parameterized Constructor
+    Airlift(Player &player, int &armies, Territory &source, Territory &target);
 
-    //Copy constructor
-    Airlift(const Airlift& airlift);
+    // Copy constructor
+    Airlift(const Airlift &airlift);
 
-    //Destructor
+    // Destructor
     ~Airlift();
 
-    //Checks if a Bomb order is valid
+    // Checks if a Bomb order is valid
     bool validate() override;
 
-    //Executes a Bomb order
+    // Executes a Bomb order
     bool execute() override;
 
-    //Defining the output operator
-    friend ostream& operator<<(ostream& strm, const Airlift& airlift);
+    // Defining the output operator
+    friend ostream &operator<<(ostream &strm, const Airlift &airlift);
 
-    //Defining the assignment operator
-    Airlift& operator=(const Airlift& airlift);
+    // Defining the assignment operator
+    Airlift &operator=(const Airlift &airlift);
 
-    // Override stringToLog class from Order
-    string stringToLog() const override;
+    // Modifications made by Tala
+    inline void Notify(ILoggable *airlift) override
+    {
+        LogObserver logObserver;
+        logObserver.Update(airlift);
+    }
+    // Modifications made by Tala
+    inline std::string stringToLog() override
+    {
+        std::cout << "Airlift order executed." << std::endl;
+        return "Airlift order executed.\n";
+    }
 };
 
-class Bomb : public Order {
+class Bomb : public Order
+{
 
 private:
-    Territory* target;
+    Territory *target;
 
 public:
-    //Default constructor
+    // Default constructor
     Bomb();
 
-    //Parameterized Constructor
-    Bomb(Player& player, Territory& target);
+    // Parameterized Constructor
+    Bomb(Player &player, Territory &target);
 
-    //Copy constructor
-    Bomb(const Bomb& bomb);
+    // Copy constructor
+    Bomb(const Bomb &bomb);
 
-    //Destructor
+    // Destructor
     ~Bomb();
 
-    //Checks if a Bomb order is valid
+    // Checks if a Bomb order is valid
     bool validate() override;
 
-    //Executes a Bomb order
+    // Executes a Bomb order
     bool execute() override;
 
-    //Defining the output operator
-    friend ostream& operator<<(ostream& strm, const Bomb& bomb);
+    // Defining the output operator
+    friend ostream &operator<<(ostream &strm, const Bomb &bomb);
 
-    //Defining the assignment operator
-    Bomb& operator=(const Bomb& bomb);
+    // Defining the assignment operator
+    Bomb &operator=(const Bomb &bomb);
 
-    // Override stringToLog class from Order
-    string stringToLog() const override;
-
+    // Modifications made by Tala
+    inline void Notify(ILoggable *bomb) override
+    {
+        LogObserver logObserver;
+        logObserver.Update(bomb);
+    }
+    // Modifications made by Tala
+    inline std::string stringToLog() override
+    {
+        std::cout << "Bomb order executed." << std::endl;
+        return "Bomb order executed.\n";
+    }
 };
 
-class Negotiate : public Order {
+class Negotiate : public Order
+{
 
 private:
-    Player* enemy;
+    Player *enemy;
 
 public:
-    //Default constructor
+    // Default constructor
     Negotiate();
 
-    //Parameterized Constructor
-    Negotiate(Player& player, Player& enemy);
+    // Parameterized Constructor
+    Negotiate(Player &player, Player &enemy);
 
-    //Copy constructor
-    Negotiate(const Negotiate& negotiate);
+    // Copy constructor
+    Negotiate(const Negotiate &negotiate);
 
-    //Destructor
+    // Destructor
     ~Negotiate();
 
-    //Checks if a Bomb order is valid
+    // Checks if a Bomb order is valid
     bool validate() override;
 
-    //Executes a Bomb order
+    // Executes a Bomb order
     bool execute() override;
 
-    //Defining the output operator
-    friend ostream& operator<<(ostream& strm, const Negotiate& negociate);
+    // Defining the output operator
+    friend ostream &operator<<(ostream &strm, const Negotiate &negociate);
 
-    //Defining the assignment operator
-    Negotiate& operator=(const Negotiate& negociate);
+    // Defining the assignment operator
+    Negotiate &operator=(const Negotiate &negociate);
 
-    // Override stringToLog class from Order
-    string stringToLog() const override;
+    // Modifications made by Tala
+    inline void Notify(ILoggable *nego) override
+    {
+        LogObserver logObserver;
+        logObserver.Update(nego);
+    }
 
+    // modifications made by Tala
+    inline std::string stringToLog() override
+    {
+        std::cout << "Negotiate order executed." << std::endl;
+        return "Negotiate order executed.\n";
+    }
 };
 
-class OrderList : public Subject, public ILoggable {
+class OrderList : public Subject, public ILoggable
+{
 
 private:
-    vector<Order*>* orders;
+    vector<Order *> *orders;
 
 public:
-    //Constructor for the orders list
+    // Constructor for the orders list
     OrderList();
 
-    //Copy constructor
-    OrderList(const OrderList& orders);
+    // Copy constructor
+    OrderList(const OrderList &orders);
 
-    //Destructor for the orders list
+    // Destructor for the orders list
     ~OrderList();
 
-    //Move function to move order in the list of orders
+    // Move function to move order in the list of orders
     void move(int startPosition, int EndPosition);
 
-    //Remove function to delete an order from the list
+    // Remove function to delete an order from the list
     void remove(int position);
 
-    //Getter for orders list
-    vector<Order*>* getOrderList() const;
+    // Getter for orders list
+    vector<Order *> *getOrderList() const;
 
-    //Defining the assignment operator
-    OrderList& operator=(const OrderList& ordersList);
+    // Defining the assignment operator
+    OrderList &operator=(const OrderList &ordersList);
 
-    //Defining the output operator
-    friend ostream& operator<<(ostream& out, const OrderList& orderlist);
+    // Defining the output operator
+    friend ostream &operator<<(ostream &out, const OrderList &orderlist);
 
-    //Method to add order to the orders list
-    void add(Order* order);
+    // Method to add order to the orders list
+    void add(Order *order);
 
-    // Override from ILoggable
-    string stringToLog() const override;
+    // Modifications made by Tala
+    inline void Notify(ILoggable *orderList) override
+    {
+        LogObserver logObserver;
+        logObserver.Update(orderList);
+    }
+    // Modifications made by Tala
+    inline std::string stringToLog() override
+    {
+        std::cout << "Order added to the order list: " << *(getOrderList()->at(0)) << std::endl;
+        std::string str;
+        std::stringstream ss;
+        ss << *(getOrderList()->at(0));
+        ss >> str;
+        return "Order added to the order list: " + str + ".\n";
+    }
 };
-
-
