935277488ed512c176dea8e22670e83f25a5e698
Tala Sleman <92806751+talasleman@users.noreply.github.com>
2023-04-14 14:01:55 -0400

    Update Player.h

--- COMP345-GroupProject/Player.h
+++ COMP345-GroupProject/Player.h
@@ -3,6 +3,7 @@
 #include "Map.cpp"
 #include "Card.cpp"
 #include "Orders.cpp"
+#include "PlayerStrategies.h" // Include PlayerStrategies.h
 
 class Player
 {
@@ -16,30 +17,34 @@ private:
     // Divide territories method
     void divideTerritories();
 
+    PlayerStrategy *strategy; // Add PlayerStrategy data member
+
 public:
     // Constructor
-    Player(std::string playerName);
+    Player(std::string playerName, PlayerStrategy *strategy);
 
     // Destructor
     ~Player();
 
     // ToDefend and ToAttack methods
     std::vector<Territory *> territories;
-    std::vector<Territory *> toDefend();
-    std::vector<Territory *> toAttack();
+    std::vector<Territory *> toDefend() { return strategy->toDefend(this); } // Delegate to the corresponding PlayerStrategy method
+    std::vector<Territory *> toAttack() { return strategy->toAttack(this); } // Delegate to the corresponding PlayerStrategy method
 
     // IssueOrder method
-    void issueOrder(std::string);
+    void issueOrder(std::string orderType) { strategy->issueOrder(this, orderType); } // Delegate to the corresponding PlayerStrategy method
 
     // Accessors
     std::string getName() const { return *playerName; }
     std::vector<Territory *> getTerritories() const { return territories; }
     OrderList *getOL() const { return OL; }
     Hand *getHand() const { return hand; }
+    PlayerStrategy *getStrategy() const { return strategy; } // Add accessor for PlayerStrategy
 
     // Mutators
     void setName(const std::string &newName) { *playerName = newName; }
     void setTerritories(const std::vector<Territory *> &newTerritories) { territories = newTerritories; }
     void setOL(OrderList *newOL) { OL = newOL; }
     void setHand(Hand *newHand) { hand = newHand; }
+    void setStrategy(PlayerStrategy *newStrategy) { strategy = newStrategy; } // Add mutator for PlayerStrategy
 };
