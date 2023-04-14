d80a41ef411f5566d623ee0bf0e430c3ee2e36ea
Tala Sleman <92806751+talasleman@users.noreply.github.com>
2023-04-14 13:49:35 -0400

    Update CommandProcessor.h

--- COMP345-GroupProject/CommandProcessor.h
+++ COMP345-GroupProject/CommandProcessor.h
@@ -1,5 +1,5 @@
 #include "GameEngine.h"
-
+#include "LoggingObserver.h"
 #include <string>
 #include <vector>
 #include <fstream>
@@ -8,7 +8,8 @@ using namespace std;
 #ifndef COMMANDPROCESSOR_H
 #define COMMANDPROCESSOR_H
 
-class Command {
+class Command : public Subject, public ILoggable
+{
 private:
     string commandString;
     string name;
@@ -17,36 +18,40 @@ public:
     Command();
     string effectString;
     bool hasArgument(string arg) const;
-    void outputCommand(const Command& cmd);
+    void outputCommand(const Command &cmd);
     string getName(Command cmd);
     Command(const string name);
-    void saveEffect(const std::string& effStr);
-    string getEffectString(Command& cmd) const;
+    void saveEffect(const std::string &effStr);
+    string getEffectString(Command &cmd) const;
+    void Notify(ILoggable *) override;
+    string stringToLog() override;
 };
 
-
-
-class CommandProcessor : public Command {
+class CommandProcessor : public Command
+{
 private:
     vector<Command> commands;
     string readCommand();
-    void saveCommand(const std::string& cmdStr);
+    void saveCommand(const std::string &cmdStr);
 
 public:
-    //std::vector<std::string> getEffects(const Command& cmd);
-    //void outputCommand(const Command& cmd);
+    // std::vector<std::string> getEffects(const Command& cmd);
+    // void outputCommand(const Command& cmd);
     CommandProcessor();
     void validate(Command cmd);
     Command getCommand();
     bool CommandProcessor::isValidTournamentCommand(Command cmd);
+    void Notify(ILoggable *) override;
+    string stringToLog() override;
 };
 
-
-
-class FileCommandProcessorAdapter : public CommandProcessor {
+class FileCommandProcessorAdapter : public CommandProcessor
+{
 public:
-    FileCommandProcessorAdapter(const string& fileName);
+    FileCommandProcessorAdapter(const string &fileName);
     void processCommands();
+    void Notify(ILoggable *) override;
+    string stringToLog() override;
 
 private:
     ifstream fileStream;
