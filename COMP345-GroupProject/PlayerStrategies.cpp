f3892a8c52d7928adca4786edf156f859269f24c
Tala Sleman <92806751+talasleman@users.noreply.github.com>
2023-04-16 09:59:04 -0400

    Update PlayerStrategies.cpp

--- COMP345-GroupProject/PlayerStrategies.cpp
+++ COMP345-GroupProject/PlayerStrategies.cpp
@@ -1,1 +1,66 @@
+#include "PlayerStrategies.h";
 
+// Human Player
+void HumanPlayerStrategy::issueOrder()
+{
+}
+
+std::vector<Territory *> HumanPlayerStrategy::toAttack()
+{
+}
+
+std::vector<Territory *> HumanPlayerStrategy::toDefend()
+{
+}
+
+// Agressive player
+void AggressivePlayerStrategy::issueOrder()
+{
+}
+
+std::vector<Territory *> AggressivePlayerStrategy::toAttack()
+{
+}
+
+std::vector<Territory *> AggressivePlayerStrategy::toDefend()
+{
+}
+
+// Benevolent player
+void BenevolentPlayerStrategy::issueOrder()
+{
+}
+
+std::vector<Territory *> BenevolentPlayerStrategy::toAttack()
+{
+}
+
+std::vector<Territory *> BenevolentPlayerStrategy::toDefend()
+{
+}
+
+// Neutral player
+void NeutralPlayerStrategy::issueOrder()
+{
+}
+
+std::vector<Territory *> NeutralPlayerStrategy::toAttack()
+{
+}
+
+std::vector<Territory *> NeutralPlayerStrategy::toDefend()
+{
+}
+
+// Cheater player
+void CheaterPlayerStrategy::issueOrder()
+{
+}
+
+std::vector<Territory *> CheaterPlayerStrategy::toAttack()
+{
+}
+
+std::vector<Territory *> CheaterPlayerStrategy::toDefend()
+{
+}
