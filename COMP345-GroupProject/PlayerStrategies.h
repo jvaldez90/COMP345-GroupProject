e01d880755d1e29110ba3993f0080d6d02004d99
Tala Sleman <92806751+talasleman@users.noreply.github.com>
2023-04-16 09:36:43 -0400

    Update PlayerStrategies.h

--- COMP345-GroupProject/PlayerStrategies.h
+++ COMP345-GroupProject/PlayerStrategies.h
@@ -4,49 +4,49 @@
 class PlayerStrategy
 {
 public:
+    Player *p;
     virtual ~PlayerStrategy() {}
-    virtual void issueOrder(Player *player, std::string orderType) = 0;
-    virtual std::vector<Territory *> toAttack(Player *player) = 0;
-    virtual std::vector<Territory *> toDefend(Player *player) = 0;
+    virtual void issueOrder() = 0;
+    virtual std::vector<Territory *> toAttack() = 0;
+    virtual std::vector<Territory *> toDefend() = 0;
 };
 
 class HumanPlayerStrategy : public PlayerStrategy
 {
 public:
-    void issueOrder(Player *player, std::string orderType) override;
-    std::vector<Territory *> toAttack(Player *player) override;
-    std::vector<Territory *> toDefend(Player *player) override;
+    void issueOrder() override;
+    std::vector<Territory *> toAttack() override;
+    std::vector<Territory *> toDefend() override;
 };
 
 class AggressivePlayerStrategy : public PlayerStrategy
 {
 public:
-    void issueOrder(Player *player, std::string orderType) override;
-    std::vector<Territory *> toAttack(Player *player) override;
-    std::vector<Territory *> toDefend(Player *player) override;
+    void issueOrder() override;
+    std::vector<Territory *> toAttack() override;
+    std::vector<Territory *> toDefend() override;
 };
 
 class BenevolentPlayerStrategy : public PlayerStrategy
 {
 public:
-    void issueOrder(Player *player, std::string orderType) override;
-    std::vector<Territory *> toAttack(Player *player) override;
-    std::vector<Territory *> toDefend(Player *player) override;
+    void issueOrder() override;
+    std::vector<Territory *> toAttack() override;
+    std::vector<Territory *> toDefend() override;
 };
 
 class NeutralPlayerStrategy : public PlayerStrategy
 {
 public:
-    void issueOrder(Player *player, std::string orderType) override;
-    std::vector<Territory *> toAttack(Player *player) override;
-    std::vector<Territory *> toDefend(Player *player) override;
+    void issueOrder() override;
+    std::vector<Territory *> toAttack() override;
+    std::vector<Territory *> toDefend() override;
 };
 
 class CheaterPlayerStrategy : public PlayerStrategy
 {
 public:
-    void issueOrder(Player *player, std::string orderType) override;
-    std::vector<Territory *> toAttack(Player *player) override;
-    std::vector<Territory *> toDefend(Player *player) override;
+    void issueOrder() override;
+    std::vector<Territory *> toAttack() override;
+    std::vector<Territory *> toDefend() override;
 };
-
